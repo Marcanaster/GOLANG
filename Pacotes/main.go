8822eddb4c5741272de78591e3c7113ec98bea98
Marcos Reis <marcanaster@gmail.com>
2023-09-25 22:06:19 -0300

    instalar e usar dependencia externa

--- Pacotes/main.go
+++ Pacotes/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Olá mundo !")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("marcnaastermail.com")
+	fmt.Println(erro)
 }
