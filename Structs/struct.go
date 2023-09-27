5eae176088f6fb3cf9fd5abaa916f0f4e88472fe
Marcos Reis <marcanaster@gmail.com>
2023-09-27 07:50:50 -0300

    struct

---
+++ Structs/struct.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome  string
+	idade int8
+	endereco endereco
+}
+
+type endereco struct{
+	logradouro string
+	numero int16
+}
+
+func main() {
+	
+	var u usuario
+	u.nome = "Marcos Reis"
+	u.idade = 49
+	fmt.Println(u)
+
+	endereco := endereco{"Rua Manoel Vieiro", 201}
+
+	usuario2 := usuario{"Marcos Reis", 21, endereco}
+	fmt.Println(usuario2)
+
+	usuario3 := usuario{nome:"Marcos Reis"}
+	fmt.Println(usuario3)
+}
